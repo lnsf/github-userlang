44c0e880c24d7983acff70dd30b7adb2af93b019
lnsf <lnsf03@gmail.com>
2019-10-17 18:02:13 +0900

    Feature: Succeed to access to GitHub

--- github_userlang/main.py
+++ github_userlang/main.py
@@ -1,5 +1,18 @@
+from github import Github
+from getpass import getpass
+
+
 def main():
-  print("Hello World")
+    u = input("YOUR GitHub username : ")
+    p = getpass()
+
+    g = Github(login_or_token=u, password=p)
+
+    g_username = input("username to search : ")
+    g_user = g.get_user(login=g_username)
+
+    print(g_user.bio)
+
 
 if __name__ == "__main__":
-  main()
+    main()
