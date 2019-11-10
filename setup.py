de009a488c6994a7181ff39e84dd9fd7d624c4b4
lnsf <lnsf03@gmail.com>
2019-11-10 22:10:11 +0900

    add colors data

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 import setuptools
+import os
+import subprocess
 
 setuptools.setup()
+
+if os.path.exists(os.getcwd() + '/github-colors'):
+    os.chdir('./github-colors')
+    subprocess.run(['git', 'pull'])
+else:
+    subprocess.run(
+        ['git', 'clone', 'https://github.com/ozh/github-colors',
+            os.getcwd() + '/github-colors']
+    )
